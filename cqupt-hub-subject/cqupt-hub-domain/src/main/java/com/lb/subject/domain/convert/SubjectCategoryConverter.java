fa1039e94f530debaf3057cb5b2629041f399b8f
NaraSky <RainSoul1120@outlook.com>
2024-11-28 19:47:47 +0800

    bug:修复转换类中@Mapper导错包的问题

--- cqupt-hub-subject/cqupt-hub-domain/src/main/java/com/lb/subject/domain/convert/SubjectCategoryConverter.java
+++ cqupt-hub-subject/cqupt-hub-domain/src/main/java/com/lb/subject/domain/convert/SubjectCategoryConverter.java
@@ -2,7 +2,8 @@ package com.lb.subject.domain.convert;
 
 import com.lb.subject.domain.entity.SubjectCategoryBO;
 import com.lb.subject.infra.basic.entity.SubjectCategory;
-import org.apache.ibatis.annotations.Mapper;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
